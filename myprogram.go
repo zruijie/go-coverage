ce789a3398307eaef917af500891a949d614682e
zhangchengcheng <943420582@qq.com>
2023-09-25 20:35:36 +0800

    update

--- myprogram.go
+++ myprogram.go
@@ -11,8 +11,11 @@ import (
 func main() {
 	fmt.Printf("I say %q and %q\n", quote.Hello(), greetings.Goodbye())
 
-	fmt.Printf("Is 1001 a big number? %v\n", mymath.IsBigNum(1001))
+	// fmt.Printf("Is 1001 a big number? %v\n", mymath.IsBigNum(1001))
 	// fmt.Printf("Is 999 a big number? %v\n", mymath.IsBigNum(999))
 
 	fmt.Printf("Is 1001 a small number? %v\n", mymath.IsSmallNum(1001))
+	fmt.Printf("Is 999 a small number? %v\n", mymath.IsSmallNum(999))
+
+	fmt.Printf("...")
 }
